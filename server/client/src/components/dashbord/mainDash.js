b77aaadb635ec81da813964215cb2cc1d2c6de36
imanejarrari <benjarrariimane@gmail.com>
2024-01-28 00:44:06 +0100

    dashbord

---
+++ server/client/src/components/dashbord/mainDash.js
@@ -0,0 +1,16 @@
+import Sidebar from '../side-bar/Sidebar';
+
+
+const  MainDash = () => {
+    return (
+        <div className="main-page">
+            <Sidebar />
+           
+           
+            
+        
+        </div>
+    );
+}
+
+export default MainDash; 
